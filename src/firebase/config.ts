cac72aeaefa7acdbc57df715dac3ae763fee69df
katrin pake k <kaatrinn24@gmail.com>
2025-12-11 05:13:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7716332540-b041e",
+  "appId": "1:1045059493648:web:852f449226a5227532cf95",
+  "apiKey": "AIzaSyDuD1l9kf-dz9SC9vTGa2_yEgvWhU3f9yg",
+  "authDomain": "studio-7716332540-b041e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1045059493648"
+};
